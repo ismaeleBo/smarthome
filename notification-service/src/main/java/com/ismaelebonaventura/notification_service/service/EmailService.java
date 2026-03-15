e375ae882b66026f40eb4941a1b1056c4ef93abb
carlo_ismaele.bonaventura <carlo_ismaele.bonaventura@consulenti.sella.it>
2026-03-15 15:03:03 +0100

    feat: (notification-service) add mailer

---
+++ notification-service/src/main/java/com/ismaelebonaventura/notification_service/service/EmailService.java
@@ -0,0 +1,24 @@
+package com.ismaelebonaventura.notification_service.service;
+
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class EmailService {
+
+    private final JavaMailSender mailSender;
+
+    public void sendEmail(String emailAddress, String subject, String body) {
+
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setTo(emailAddress);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+    }
+}
